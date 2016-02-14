f41d4d7fe8d64f678df5ee6d7cf84039f7d498f6
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2016-02-14 05:26:43 +0500

    merge

--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { hashHistory } from 'react-router';
+import { syncHistory } from 'react-router-redux';
 import rootReducer from '../reducers';
 
+const reduxRouterMiddleware = syncHistory(hashHistory);
 const enhancer = compose(
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, reduxRouterMiddleware)
 );
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
+
+  reduxRouterMiddleware.listenForReplays(store);
+
+  return store;
 }
